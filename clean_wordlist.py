5c505ce516e1fcb85b5903757959b2afefb36d22
Aveek Saha <aveek.s98@gmail.com>
2021-11-08 12:21:40 +0530

    Add valid guesses

--- clean_wordlist.py
+++ clean_wordlist.py
@@ -1,4 +1,16 @@
 import csv
-with open ('wordlist.txt', 'r', encoding='utf8') as f:
-    wordlist = [[row[0], row[2], row[3]] for row in csv.reader(f,delimiter=' ')]
-    print (wordlist)
+from turtle import pos
+with open ('wordlist_all.txt', 'r', encoding='utf8') as f:
+    # wordlist = [[row[0], row[2]] for row in csv.reader(f,delimiter=' ')]
+    wordlist = [row[0] for row in csv.reader(f,delimiter=' ')]
+    # print (wordlist)
+
+with open ('possible_answers.txt', 'r', encoding='utf8') as f:
+    possible = [row for row in csv.reader(f,delimiter=',')][0]
+
+    print(len(possible))
+
+for word in possible:
+    if word not in wordlist:
+        print(word)
+        break
