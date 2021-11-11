8511cfd91fdba9238e7ab7977de164820c121ecd
Aveek Saha <aveek.s98@gmail.com>
2021-11-11 18:43:02 +0530

    Compute tfidf

--- clean_wordlist.py
+++ clean_wordlist.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import math
 import os
 
 with open(os.path.join('datasets', 'wordlist_all.txt'), 'r', encoding='utf8') as f:
@@ -15,14 +16,31 @@ with open(os.path.join('datasets', 'valid_guesses.txt'), 'r', encoding='utf8') a
 
     print(len(valid))
 
-possible_words = {}
-for word in valid:
-    if word in wordlist:
-        possible_words[word] = wordlist[word]
+# possible_words = {}
+# for word in valid:
+#     if word in wordlist:
+#         possible_words[word] = wordlist[word]
 
-    else:
-        possible_words[word] = "1"
+#     else:
+#         possible_words[word] = "1"
 
 
-with open(os.path.join('datasets', 'valid_word_freq.json'), "w") as outfile:
-    json.dump(possible_words, outfile, indent=4)
+# with open(os.path.join('datasets', 'valid_word_freq.json'), "w") as outfile:
+#     json.dump(possible_words, outfile, indent=4)
+
+
+TOTAL_WORDS = 3000000
+TOTAL_ARTICLES = 6000000
+
+def inv_tfidf(word_freq, doc_freq):
+    tf = word_freq/TOTAL_WORDS
+
+    idf = math.log(TOTAL_ARTICLES/(doc_freq + 1))
+
+    tfidf = tf * idf
+
+    return tfidf
+
+print(inv_tfidf(1, 1))
+print(inv_tfidf(6296, 5009))
+print(inv_tfidf(1220752, 890394))
