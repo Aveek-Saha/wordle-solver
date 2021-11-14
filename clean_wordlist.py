9c69e88213f1105b888c0112ffdf725edd0e639c
Aveek Saha <aveek.s98@gmail.com>
2021-11-14 23:40:17 +0530

    Change freq metric

--- clean_wordlist.py
+++ clean_wordlist.py
@@ -3,32 +3,6 @@ import json
 import math
 import os
 
-with open(os.path.join('datasets', 'wordlist_all.txt'), 'r', encoding='utf8') as f:
-    # wordlist = [[row[0], row[2]] for row in csv.reader(f,delimiter=' ')]
-    wordlist = {}
-    for row in csv.reader(f, delimiter=' '):
-        wordlist[row[0]] = row[2]
-
-    # print (wordlist)
-
-with open(os.path.join('datasets', 'valid_guesses.txt'), 'r', encoding='utf8') as f:
-    valid = [row for row in csv.reader(f, delimiter=',')][0]
-
-    print(len(valid))
-
-# possible_words = {}
-# for word in valid:
-#     if word in wordlist:
-#         possible_words[word] = wordlist[word]
-
-#     else:
-#         possible_words[word] = "1"
-
-
-# with open(os.path.join('datasets', 'valid_word_freq.json'), "w") as outfile:
-#     json.dump(possible_words, outfile, indent=4)
-
-
 TOTAL_WORDS = 3000000
 TOTAL_ARTICLES = 6000000
 
@@ -41,6 +15,34 @@ def inv_tfidf(word_freq, doc_freq):
 
     return tfidf
 
-print(inv_tfidf(1, 1))
-print(inv_tfidf(6296, 5009))
-print(inv_tfidf(1220752, 890394))
+with open(os.path.join('datasets', 'wordlist_all.txt'), 'r', encoding='utf8') as f:
+    # wordlist = [[row[0], row[2]] for row in csv.reader(f,delimiter=' ')]
+    wordlist = {}
+    for row in csv.reader(f, delimiter=' '):
+        wordlist[row[0]] = [int(row[2]), int(row[3])]
+
+    # print (wordlist)
+
+with open(os.path.join('datasets', 'valid_guesses.txt'), 'r', encoding='utf8') as f:
+    valid = [row for row in csv.reader(f, delimiter=',')][0]
+
+    print(len(valid))
+
+possible_words = {}
+for word in valid:
+    if word in wordlist:
+        possible_words[word] = inv_tfidf(wordlist[word][0], wordlist[word][1])
+
+    else:
+        possible_words[word] = inv_tfidf(1, 1)
+
+sorted_possible_words = dict(sorted(possible_words.items(), key=lambda item: item[1], reverse=True))
+
+with open(os.path.join('datasets', 'valid_word_scores.json'), "w") as outfile:
+    json.dump(sorted_possible_words, outfile, indent=4)
+
+
+
+# print(inv_tfidf(1, 1))
+# print(inv_tfidf(6296, 5009))
+# print(inv_tfidf(1220752, 890394))
