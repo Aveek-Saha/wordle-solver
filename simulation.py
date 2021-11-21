b8cbd2606cb7277466bcc2673ebf4c4ba5a80f12
Aveek Saha <aveek.s98@gmail.com>
2021-11-21 11:57:33 +0530

    Simulate solutions

--- simulation.py
+++ simulation.py
@@ -3,13 +3,20 @@ import os
 import csv
 from itertools import product
 from tqdm import tqdm
+from operator import itemgetter
 
 from generate_entropy import *
 from wordle import *
 
 def generate_guess(previous_board, previous_guess, wordlist, turn):
     if turn == 0:
-        return guess
+        return guess, wordlist
+    
+    if turn == 1:
+        board_name = "".join([str(int) for int in list(previous_board)])
+        guess_list = second_guess_scores[board_name]
+        filtered_words = {guess["word"]: guess["word"] for guess in guess_list}
+        return max(guess_list, key=itemgetter('score'))["word"], filtered_words
     
     else:
         filtered_words = filter_words(previous_board, previous_guess, wordlist)
@@ -17,8 +24,9 @@ def generate_guess(previous_board, previous_guess, wordlist, turn):
         for word in filtered_words:
             entropy = get_entropy(word, filtered_words, combs)
             next_guess[word] = entropy * data[word]
+        filtered_words = {word: word for word in filtered_words}
         sorted_next_guess = list(dict(sorted(next_guess.items(), key=lambda item: item[1], reverse=True)).keys())[0]
-        return sorted_next_guess
+        return sorted_next_guess, filtered_words
 
 with open(os.path.join('datasets', 'possible_answers.txt'), 'r', encoding='utf8') as f:
     words = [row for row in csv.reader(f, delimiter=',')][0]
@@ -31,6 +39,9 @@ with open(os.path.join('datasets', 'first_guess_scores_2.json'), "r") as file:
     wordlist = {}
     for word in first_guess_list:
         wordlist[word] = word
+
+with open(os.path.join('datasets', 'second_guess_scores.json'), "r") as file:
+    second_guess_scores = json.load(file)
 
 # word = random.choice(words)
 
@@ -62,23 +73,25 @@ combs = list(product([0, 1, 2], repeat=5))
 
 score_total = 0
 failed_games = 0
-total_games = len(words)
-for word in tqdm(words):
+# total_games = len(words)
+total_games = 10
+record = {}
+record["games"] = []
+for word in tqdm(words[:10]):
     board = [0, 0, 0, 0, 0]
     guess = list(first_guess_list.keys())[0]
     score = 1
+    filtered_wordlist = wordlist
+    game = {}
+    # game["answer"] = 
 
     for turn in range(6):
-
         previous_guess = guess
-        guess = generate_guess(board, previous_guess, wordlist, turn)
-
+        guess, filtered_wordlist = generate_guess(board, previous_guess, filtered_wordlist, turn)
         board = check_guess(word, guess)
-
         if board == [2, 2, 2, 2, 2]:
             break
         score += 1
-
         if turn == 5:
             score = 0
     
