ab4afd56bc6249ba1dac655923c8135e442b4079
Aveek Saha <aveek.s98@gmail.com>
2021-11-24 23:21:58 +0530

    Add analysis of results

---
+++ analysis.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import json
+import os
+from collections import Counter
+
+import pandas as pd
+
+with open(os.path.join('datasets', 'simulation_results.json'), "r") as file:
+    simulation_results = json.load(file)
+
+results = simulation_results["games"]
+scores = []
+
+for result in results:
+    scores.append(str(results[result]["score"]))
+
+count = Counter(scores)
+df = pd.DataFrame.from_dict(count, orient='index').sort_index(axis = 0)
+df.plot(kind='bar')
+plt.show()
+
+# print(df)
