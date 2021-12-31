62901f816bee3d249bcbd87651dc2836aa82f99b
Aveek Saha <aveek.s98@gmail.com>
2021-12-31 17:52:30 +0530

    Modify section for readme

--- update_wordle.py
+++ update_wordle.py
@@ -1,7 +1,25 @@
 import re
+import os
+import json
 
-with open('README.md', 'r', encoding='utf8') as f:
-    text = f.read()
+DATASET_DIR = 'datasets'
+EXPERIMENT_DIR = 'filtered'
 
-match = re.search(r'## Today\'s Wordle(.*?)# Rules', text, re.DOTALL).group(1)
-print(match)
+# with open('README.md', 'r', encoding='utf8') as f:
+#     text = f.read()
+
+# match = re.search(r'## Today\'s Wordle(.*?)<details>', text, re.DOTALL).group(1)
+# print(match)
+
+RESULTS = os.path.join(DATASET_DIR, EXPERIMENT_DIR, 'simulation_results_scaled_tf.json')
+
+with open(RESULTS, "r", encoding='utf8') as file:
+    res = json.load(file)
+
+day = "261"
+
+current_answer = res["games"][day]
+
+sol = current_answer["share"].split("\n\n")
+
+print(sol[0] + "\n\n" + " <br>\n".join(sol[1].split("\n")))
