7d5e18d1f32b6c0bba7dcce7d79f6b374b7df777
Aveek Saha <aveek.s98@gmail.com>
2022-01-04 18:36:35 +0530

    Update both

--- update_wordle.py
+++ update_wordle.py
@@ -15,11 +15,12 @@ with open('README.md', 'r', encoding='utf8') as f:
     text = f.read()
 
 # match = re.search(r'## Today\'s Wordle(.*?)<details>', text, re.DOTALL).group(1)
-match = re.search(r'</summary>(.*?)</pre>', text, re.DOTALL).group(1)
+# match = re.search(r'</summary>(.*?)</pre>', text, re.DOTALL).group(1)
 
 # print(match)
 
-RESULTS = os.path.join(DATASET_DIR, EXPERIMENT_DIR, 'simulation_results_scaled_tf.json')
+RESULTS = os.path.join(DATASET_DIR, EXPERIMENT_DIR,
+                       'simulation_results_scaled_tf.json')
 
 with open(RESULTS, "r", encoding='utf8') as file:
     res = json.load(file)
@@ -28,11 +29,14 @@ current_answer = res["games"][day]
 
 board = current_answer["share"].split("\n\n")
 
-share_board = "## Today's Wordle\n\n" + board[0] + "\n\n" + " <br>\n".join(board[1].split("\n")) + "\n<details>"
+share_board = "## Today's Wordle\n\n" + \
+    board[0] + "\n\n" + " <br>\n".join(board[1].split("\n")) + "\n<details>"
+guesses = "</summary>\n\nAnswer: `" + current_answer["answer"].upper(
+) + "`\n<pre>\n" + "\n".join(current_answer["guesses"]).upper() + "\n</pre>\n"
 
-# print(board[0] + "\n\n" + " <br>\n".join(board[1].split("\n")))
-# print("Answer: `" + current_answer["answer"].upper() + "`\n<pre>\n" + "\n".join(current_answer["guesses"]).upper())
+sub_board = re.sub(r'## Today\'s Wordle(.*?)<details>',
+                   share_board, text, count=1, flags=re.DOTALL)
+sub_answer = re.sub(r'</summary>(.*?)</pre>', guesses,
+                    sub_board, count=1, flags=re.DOTALL)
 
-new = re.sub(r'## Today\'s Wordle(.*?)<details>', share_board, text, count=1, flags=re.DOTALL)
-
-print(new)
+print(sub_answer)
